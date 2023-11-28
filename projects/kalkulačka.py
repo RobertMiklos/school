1affedd439a385036da11e04c52a9e63098cc373
RobertMiklos <150846977+RobertMiklos@users.noreply.github.com>
2023-11-28 11:11:45 +0100

    idk

--- projects/kalkulačka.py
+++ projects/kalkulačka.py
@@ -1,1 +1,23 @@
-print("kalkulačka")
+import random
+w, h, mines = 10, 5, 20
+
+# gene field
+field = [[0 for y in range(h)] for x in range(w)]
+
+# put mine on random position
+if mines > w * h:
+    mines = 0
+    print("Error: Too many mines.")
+for i in range(mines):
+    while True:
+        rx = random.randint(0, w - 1)
+        ry = random.randint(0, h - 1)
+        if field[rx][ry] != "M":
+            field[rx][ry] = "M"
+            break
+    
+# print field
+for y in range(h):
+    for x in range(w):
+        print(field[x][y], end="")
+    print()
