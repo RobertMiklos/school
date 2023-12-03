5dc38c163ad360db2d66a4fccfc6a5e3d9312560
RobertMiklos <150846977+RobertMiklos@users.noreply.github.com>
2023-12-03 19:42:42 +0100

    idk

---
+++ projects/turtle-tree.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+t.home()
+t.clearscreen()
+t.color("#964B00")
+t.forward(35)
+t.left(90)
+t.forward(70)
+t.color("green")
+t.right(90)
+t.forward(150)
+t.left(140)
+t.forward(70)
+
+
+t.mainloop()
