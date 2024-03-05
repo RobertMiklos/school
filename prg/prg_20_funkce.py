0b5dd621ff21c78f45b5e302d93f53dff02f9c1c
RobertMiklos <r.miklos@email.cz>
2024-03-05 15:40:51 +0100

    idk

---
+++ prg/prg_20_funkce.py
@@ -0,0 +1,7 @@
+num = [2,3,6,8,14,4]
+
+num.append(int(input("Zadej číslo: ")))
+
+print(num)
+
+# funkce .append() přídá zadanou hodnotu na konec listu
---
+++ prg/prg_23_funkce.py
@@ -0,0 +1,7 @@
+numbers = [2,3,6,8,1]
+
+numbers.insert(2,5)
+
+print(numbers)
+
+# funkce .insert() přidá číslo 5 do listu na pozici 2
