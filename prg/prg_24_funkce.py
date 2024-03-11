c1ba582c3a9ca2e5cdd50ebffc6daba99c3c0000
RobertMiklos <r.miklos@email.cz>
2024-03-11 15:19:46 +0100

    idk

---
+++ prg/prg_24_funkce.py
@@ -0,0 +1,5 @@
+letters = ["r","c","s","g"]
+print(letters.index("r"))
+print(letters.index("g"))
+
+# funkce .index() vypíše na jakým místě je zvolené písmeno
---
+++ prg/prg_26_funkce.py
@@ -0,0 +1,5 @@
+x = [3,5,11,7,8,4,5]
+x.remove(5)
+print(x)
+
+# funkce .remove() odstraní zvolenou hodnotu z listu
