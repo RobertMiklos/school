2ea9944e613498d63e80dc38b2ab456bb30a2658
RobertMiklos <r.miklos@email.cz>
2024-06-04 19:26:12 +0200

    idk

--- prg/prg_30_guessing_game.py
+++ prg/prg_30_guessing_game.py
@@ -4,19 +4,23 @@ def generate_random_number():
     number = random.randint(0, 10)
     return(number)
 
-correct_num = generate_random_number()
+corect_num = generate_random_number()
 guess = int(input("zadejte číslo:\n"))
 atemps = 0
 
 while True: 
-    if guess == correct_num:
+    if guess == corect_num:
         atemps += 1
         print("hurá!")
-        print("uhádl jsi to na " f"{atemps}" " pokusů")
+        print("uhádl jsi to na " f"{atemps}" " pokusů.")
         break
+    elif guess > corect_num:
+        print("Tvůj typ je větší.")
+    elif guess < corect_num:
+        print("Tvůj typ je menší.")
     else:
         atemps += 1
         guess = int(input("zadejte číslo:\n"))
 
 
-# hra kde kde jse hádá správné číslo
+# hra kde se hádá správné číslo
