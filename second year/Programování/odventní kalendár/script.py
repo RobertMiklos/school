e6230683529c74ee07f6bf4a4b51696243337db9
Robert Mikloš <r.miklos@email.cz>
2024-11-12 17:11:44 +0100

    updates prg

--- second year/Programování/odventní kalendár/script.py
+++ second year/Programování/odventní kalendár/script.py
@@ -1,24 +1,22 @@
 import json
 from datetime import datetime
 
-# Načteme soubor s otázkami, odpověďmi a dárky
-def load_questions():
-    with open("D:\development\school\school\2.year\Programování\002 adventní kalendár\advent_calendar.json", "r") as file:
+# json
+def json_load():
+    with open("D:\development\school\second year\Programování\odventní kalendár\odvent_calendar.json", "r") as file:
         return json.load(file)
 
-# Získáme dnešní datum
-def get_today_date():
+# datum
+def todays_date():
     today = datetime.today()
-    # Zkontrolujeme, zda je dnešní datum mezi 1. a 24. prosincem 2024
-    return today.month == 11 and 1 <= today.day <= 24
-
-# Hlavní funkce pro zobrazení otázky a vyhodnocení odpovědi
+    # na zkoušku z 12 na 11 
+    return today.month == 12 and 1 <= today.day <= 24
+ 
 def main():
-    if get_today_date():
+    if todays_date():
         today = datetime.today().day
-        questions = load_questions()
+        questions = json_load()
 
-        # Zkontrolujeme, zda existuje otázka pro dnešní den
         if str(today) in questions:
             question_data = questions[str(today)]
             print(question_data["question"])
