8d5dcef8528e0d949bed675b0bd66fd5609c8e57
Robert Mikloš <r.miklos@email.cz>
2024-11-12 17:30:53 +0100

    changes

--- second year/Programování/odventní kalendár/script.py
+++ second year/Programování/odventní kalendár/script.py
@@ -1,21 +1,22 @@
 import json
 from datetime import datetime
 
-# json
-def json_load():
-    with open("D:\development\school\second year\Programování\odventní kalendár\odvent_calendar.json", "r") as file:
-        return json.load(file)
-
 # datum
 def todays_date():
     today = datetime.today()
-    # na zkoušku z 12 na 11 
+    # na zkoušku 11 másto 12
     return today.month == 12 and 1 <= today.day <= 24
- 
+
+# json
+def open_json():
+    with open("calendar.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
 def main():
     if todays_date():
         today = datetime.today().day
-        questions = json_load()
+        questions = open_json()
 
         if str(today) in questions:
             question_data = questions[str(today)]
@@ -23,14 +24,13 @@ def main():
             user_answer = input("Tvoje odpověď: ")
 
             if user_answer.strip() == question_data["answer"]:
-                print(f"Za tvou šikovnost tu máš ode mě {question_data['gift']}.")
+                print(f"Jako odměnu získáváš {question_data['gift']}.")
             else:
                 print("To se nepovedlo, ale nevadí, můžeš to zkusit znovu.")
         else:
-            print("Dnešní den není v adventním kalendáři.")
+            print("Dnešní den není v kalendáři.")
     else:
-        print("Dnešní den není v adventním kalendáři.")
+        print("Dnešní den není v kalendáři.")
 
 if __name__ == "__main__":
     main()
-
